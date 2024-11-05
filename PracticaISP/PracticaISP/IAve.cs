83c40574e652accb8562dfe8247fa92bcfe26954
Luis Alberto Mesili <42626226+Guitarra2428@users.noreply.github.com>
2024-11-05 16:19:53 -0400

    Add files via upload

---
+++ PracticaISP/PracticaISP/IAve.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaISP
+{
+    public interface IAve: IVolar, ICantar, IPonerHuevos
+    {
+       
+    }  
+    public interface IVolar
+    {
+        void Volar();
+       
+
+    }  
+    public interface ICantar
+    {       
+        void Cantar();
+       
+
+    }  
+    
+    public interface IPonerHuevos
+    {       
+        void PonerHuevos();      
+
+    } 
+    public interface INadar
+    {
+        void Nadar();
+     
+    }   
+    public interface ICazar
+    {
+        void Cazar();
+
+    }
+    ///Ejemplo de una gallina
+    ///
+    public class Gallina : IAve
+    {
+        public void Cantar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PonerHuevos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Volar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PracticaISP/PracticaISP/Program.cs
@@ -0,0 +1,2 @@
+﻿CA// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
